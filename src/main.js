2d8096f91d6396014165d30a971f14931a386862
andykuo1 <andykuo1supergreen@gmail.com>
2021-11-07 09:57:33 -0800

    Bump version to 1.0.14

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import {
 } from './inventory/ItemBuilder.js';
 import { loadFromLocalStorage, saveToLocalStorage } from './inventory/InventoryLoader.js';
 
-const APP_VERSION = '1.0.13';
+const APP_VERSION = '1.0.14';
 
 window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#appVersion').textContent = `v${APP_VERSION}`;
