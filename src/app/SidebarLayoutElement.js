819c5ddf165954d23d3787543d2c13a337d26ec6
andykuo1 <andykuo1supergreen@gmail.com>
2021-12-01 08:31:45 -0800

    Add sidebar layout

---
+++ src/app/SidebarLayoutElement.js
@@ -0,0 +1,84 @@
+const INNER_HTML = /* html */ `
+<div id="sidebar">
+  <slot name="sidebar"></slot>
+</div>
+<div id="viewport">
+  <slot></slot>
+</div>
+`;
+const INNER_STYLE = /* css */ `
+:host {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+  --background-color: #333333;
+}
+
+#sidebar {
+  background-color: var(--background-color);
+  padding: 0.5em 1em;
+  min-width: 4em;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+
+#sidebar ::slotted(*) {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+}
+
+#viewport {
+  flex: 1;
+}
+`;
+
+export class SidebarLayoutElement extends HTMLElement {
+  /** @private */
+  static get [Symbol.for('templateNode')]() {
+    const t = document.createElement('template');
+    t.innerHTML = INNER_HTML;
+    Object.defineProperty(this, Symbol.for('templateNode'), { value: t });
+    return t;
+  }
+
+  /** @private */
+  static get [Symbol.for('styleNode')]() {
+    const t = document.createElement('style');
+    t.innerHTML = INNER_STYLE;
+    Object.defineProperty(this, Symbol.for('styleNode'), { value: t });
+    return t;
+  }
+
+  static define(customElements = window.customElements) {
+    customElements.define('sidebar-layout', this);
+  }
+
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.append(this.constructor[Symbol.for('templateNode')].content.cloneNode(true));
+    shadowRoot.append(this.constructor[Symbol.for('styleNode')].cloneNode(true));
+  }
+
+  /** @protected */
+  connectedCallback() {
+  }
+
+  /** @protected */
+  disconnectedCallback() {
+  }
+
+  /**
+   * @param attribute
+   * @param previous
+   * @param value
+   * @protected
+   */
+  attributeChangedCallback(attribute, previous, value) {
+  }
+}
+SidebarLayoutElement.define();
--- src/app/index.js
+++ src/app/index.js
@@ -2,3 +2,4 @@ export { ItemBarElement } from './ItemBarElement.js';
 export { ItemBarItemElement } from './ItemBarItemElement.js';
 export { IconButtonElement } from './IconButtonElement.js';
 export { DialogPromptElement } from './DialogPromptElement.js';
+export { SidebarLayoutElement } from './SidebarLayoutElement.js';
