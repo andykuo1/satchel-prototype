e9e2cb3692488c8051bb7b8dd217ca82743cbc7a
andykuo1 <andykuo1supergreen@gmail.com>
2022-01-22 08:07:15 -0800

    Add unused item-grid for future

--- src/components/invgrid/index.js
+++ src/components/invgrid/index.js
@@ -1,3 +1,6 @@
 export { InventoryItemElement } from './InventoryItemElement.js';
 export { InvSocketElement } from './InvSocketElement.js';
 export { InvGridElement } from './InvGridElement.js';
+
+// TODO: Not yet used.
+export { ItemGridElement } from './ItemGridElement.js';
